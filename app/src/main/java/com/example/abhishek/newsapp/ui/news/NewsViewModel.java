92175153ca62a8b95e3bbd0096d7d1ce400cf2da
siddharth <sidverma24@gmail.com>
2021-11-27 22:06:17 +0530

    Commit

---
+++ app/src/main/java/com/example/abhishek/newsapp/ui/news/NewsViewModel.java
@@ -0,0 +1,37 @@
+package com.example.abhishek.newsapp.ui.news;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.example.abhishek.newsapp.data.NewsRepository;
+import com.example.abhishek.newsapp.models.Article;
+import com.example.abhishek.newsapp.models.Specification;
+
+import java.util.List;
+
+public class NewsViewModel extends AndroidViewModel {
+    private final NewsRepository newsRepository;
+
+    public NewsViewModel(@NonNull Application application) {
+        super(application);
+        newsRepository = NewsRepository.getInstance(application.getApplicationContext());
+    }
+
+    public LiveData<List<Article>> getNewsHeadlines(Specification specification) {
+        return newsRepository.getHeadlines(specification);
+    }
+
+    public LiveData<List<Article>> getAllSaved() {
+        return newsRepository.getSaved();
+    }
+
+    public LiveData<Boolean> isSaved(int articleId) {
+        return newsRepository.isSaved(articleId);
+    }
+
+    public void toggleSave(int articleId) {
+        newsRepository.removeSaved(articleId);
+    }
+}
